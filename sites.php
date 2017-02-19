5268f87bddf54ab6dcf3f4d29d09325fa7a0a5b7
qiyoo <qi364856306@sina.com>
2017-02-19 18:59:23 +0800

    testservice

---
+++ sites.php
@@ -0,0 +1,24 @@
+{
+    "sites": [
+        {
+            "Name": "²ËÄñ½Ì³Ì",
+            "Url": "www.runoob.com",
+            "Country": "CN"
+        },
+        {
+            "Name": "Google",
+            "Url": "www.google.com",
+            "Country": "USA"
+        },
+        {
+            "Name": "Facebook",
+            "Url": "www.facebook.com",
+            "Country": "USA"
+        },
+        {
+            "Name": "Î¢²©",
+            "Url": "www.weibo.com",
+            "Country": "CN"
+        }
+    ]
+}
